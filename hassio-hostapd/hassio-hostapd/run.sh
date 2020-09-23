d565996f2e9fd5d8f75e27353452ad06789bdcab
mattlongman <git@mattlongman.com>
2020-09-23 13:11:50 +0100

    Update run.sh
    - replace references to wlan0 with new variable
    - move term_handler trap down to below variable instantiation
    - generate /etc/network/interfaces (because we don't know interface name until runtime)
    - add MAC address filtering
    - sometimes the addon doesn't start without a reboot. Adding the killall hostapd before running seems to help (might still fail first time, but should start second time without need for reboot). Needs more testing.
    - add whether ssid is hidden to hostapd.conf
    - add mac address filtering/files to hostapd.conf
    - add interface name to hostapd.conf

--- hassio-hostapd/hassio-hostapd/run.sh
+++ hassio-hostapd/hassio-hostapd/run.sh
@@ -3,19 +3,13 @@
 # SIGTERM-handler this funciton will be executed when the container receives the SIGTERM signal (when stopping)
 term_handler(){
 	echo "Stopping..."
-	ifdown wlan0
-	ip link set wlan0 down
-	ip addr flush dev wlan0
+	ifdown $INTERFACE
+	ip link set $INTERFACE down
+	ip addr flush dev $INTERFACE
 	exit 0
 }
 
-# Setup signal handlers
-trap 'term_handler' SIGTERM
-
 echo "Starting..."
-
-echo "Set nmcli managed no"
-nmcli dev set wlan0 managed no
 
 CONFIG_PATH=/data/options.json
 
@@ -25,6 +19,53 @@ CHANNEL=$(jq --raw-output ".channel" $CONFIG_PATH)
 ADDRESS=$(jq --raw-output ".address" $CONFIG_PATH)
 NETMASK=$(jq --raw-output ".netmask" $CONFIG_PATH)
 BROADCAST=$(jq --raw-output ".broadcast" $CONFIG_PATH)
+INTERFACE=$(jq --raw-output ".interface" $CONFIG_PATH)
+HIDE_SSID=$(jq --raw-output ".hide_ssid" $CONFIG_PATH)
+ALLOW_MAC_ADDRESSES=$(jq --raw-output '.allow_mac_addresses | join(" ")' $CONFIG_PATH)
+DENY_MAC_ADDRESSES=$(jq --raw-output '.deny_mac_addresses | join(" ")' $CONFIG_PATH)
+
+# Set interface as wlan0 if not specified in config
+if [ ${#INTERFACE} -eq 0 ]; then
+    INTERFACE="wlan0"
+fi
+
+echo "iface $INTERFACE inet static"$'\n' >> /etc/network/interfaces
+
+echo "Set nmcli managed no"
+nmcli dev set $INTERFACE managed no
+
+# Setup signal handlers
+trap 'term_handler' SIGTERM
+
+
+### MAC address filtering
+## Allow is more restrictive, so we prioritise that and set
+## macaddr_acl to 1, and add allowed MAC addresses to hostapd.allow
+if [ ${#ALLOW_MAC_ADDRESSES} -ge 1 ]; then
+    echo "macaddr_acl=1"$'\n' >> /hostapd.conf
+    ALLOWED=($ALLOW_MAC_ADDRESSES)
+    for mac in "${ALLOWED[@]}"; do
+        echo "$mac"$'\n' >> /hostapd.allow
+    done
+    echo "accept_mac_file=/hostapd.allow"$'\n' >> /hostapd.conf
+## else set macaddr_acl to 0, and add denied MAC addresses to hostapd.deny
+    else
+        if [ ${#DENY_MAC_ADDRESSES} -ge 1 ]; then
+            echo "macaddr_acl=0"$'\n' >> /hostapd.conf
+            DENIED=($DENY_MAC_ADDRESSES)
+            for mac in "${DENIED[@]}"; do
+                echo "$mac"$'\n' >> /hostapd.deny
+            done
+            echo "deny_mac_file=/hostapd.deny"$'\n' >> /hostapd.conf
+## else set macaddr_acl to 0, with blank allow and deny files
+            else
+                echo "macaddr_acl=0"$'\n' >> /hostapd.conf
+        fi
+
+fi
+
+# Add interface to hostapd.conf
+echo "interface=$INTERFACE"$'\n' >> /hostapd.conf
 
 # Enforces required env variables
 required_vars=(SSID WPA_PASSPHRASE CHANNEL ADDRESS NETMASK BROADCAST)
@@ -35,6 +76,11 @@ for required_var in "${required_vars[@]}"; do
     fi
 done
 
+# Sanitise config value for hide_ssid
+if [ $HIDE_SSID -ne 1 ]; then
+        HIDE_SSID=0
+fi
+
 if [[ -n $error ]]; then
     exit 1
 fi
@@ -44,6 +90,7 @@ echo "Setup hostapd ..."
 echo "ssid=$SSID"$'\n' >> /hostapd.conf
 echo "wpa_passphrase=$WPA_PASSPHRASE"$'\n' >> /hostapd.conf
 echo "channel=$CHANNEL"$'\n' >> /hostapd.conf
+echo "ignore_broadcast_ssid=$HIDE_SSID"$'\n' >> /hostapd.conf
 
 # Setup interface
 echo "Setup interface ..."
@@ -53,12 +100,14 @@ echo "Setup interface ..."
 #ip addr add ${IP_ADDRESS}/24 dev wlan0
 #ip link set wlan0 up
 
+ip link set $INTERFACE down
+
 echo "address $ADDRESS"$'\n' >> /etc/network/interfaces
 echo "netmask $NETMASK"$'\n' >> /etc/network/interfaces
 echo "broadcast $BROADCAST"$'\n' >> /etc/network/interfaces
 
-ifdown wlan0
-ifup wlan0
+ip link set $INTERFACE up
+
 
 echo "Starting HostAP daemon ..."
-hostapd -d /hostapd.conf & wait ${!}
+killall hostapd; hostapd -d /hostapd.conf & wait ${!}
